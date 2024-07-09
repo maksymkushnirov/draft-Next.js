1458838e1079e887c38f005dfb950841a63456c9
maksymkushnirov <maksymkushnirov@gmail.com>
2024-07-09 17:38:50 +0300

    routing

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,11 +1,10 @@
-import 
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header>
       <h3>Header</h3>
-      <Link href="/">Homepage</Link>
-      <Link href="/blog">Blog</Link>
+      <Link href="/">Homepage</Link> <Link href="/blog">Blog</Link>
     </header>
   );
 }
