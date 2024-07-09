79314c360f7b2c08ff5aa8fba569a778e1584eb6
maksymkushnirov <maksymkushnirov@gmail.com>
2024-07-09 09:32:02 +0300

    header

---
+++ app/_components/Header.jsx
@@ -0,0 +1,11 @@
+import 
+
+export default function Header() {
+  return (
+    <header>
+      <h3>Header</h3>
+      <Link href="/">Homepage</Link>
+      <Link href="/blog">Blog</Link>
+    </header>
+  );
+}
